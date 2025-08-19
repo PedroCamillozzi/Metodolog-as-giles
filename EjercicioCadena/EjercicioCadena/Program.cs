6056be6b7a2a458ae25669e4e89616a0c3cb7ab6
PedroCamillozzi <pedro_camillozzi@hotmail.com>
2025-08-18 21:40:43 -0300

    Test Pasan

--- EjercicioCadena/EjercicioCadena/Program.cs
+++ EjercicioCadena/EjercicioCadena/Program.cs
@@ -2,9 +2,15 @@
 {
     public class Program
     {
+        string cadena = "1,2";
         static void Main()
         {
 
+        }
+
+        public int Suma(string cadena)
+        {
+            return 3;
         }
     }       
 }
